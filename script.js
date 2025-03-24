ad456ba9b4a490766e5685b6d5e904215631a037
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-03-24 10:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ function SportsCar(make, model, topSpeed) {
 	}
 }
 
+const car = new SportsCar("Ferrari","Testarossa",200);
+console.log(car.getMakeModel());
+console.log(car.getTopSpeed())
+
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
