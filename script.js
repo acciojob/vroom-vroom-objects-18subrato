319b661f5ad42acb0f25a39bdc17f612119835d8
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-03-24 10:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Complete the js code
 function Car(make, model) {
 	constructor(make,model){
-		this.make = make;
-		this.model = model;
+		this._make = make;
+		this._model = model;
 	}
 	getMakeModel(){
-		return `${this.make} ${this.model}`;
+		return `${this._make} ${this._model}`;
 	}
 }
 
@@ -13,10 +13,10 @@ function SportsCar(make, model, topSpeed) {
 	constructor(make,model,topSpeed){
 		this.make = make;
 		this.model = make;
-		this.topSpeed = topSpeed;
+		this._topSpeed = topSpeed;
 	}
 	getTopSpeed(){
-		return `${this.topSpeed}`;
+		return `${this._topSpeed}`;
 	}
 }
 
